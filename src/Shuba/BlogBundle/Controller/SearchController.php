e55db626f0b20322b30cbb1622dc6c8a2658e05d
shubaivan <shuba.ivan@yandex.ru>
2015-01-04 17:26:52 +0200

    add Controller and view

---
+++ src/Shuba/BlogBundle/Controller/SearchController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan
+ * Date: 04.01.15
+ * Time: 17:09
+ */
+
+namespace Shuba\BlogBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SearchController extends Controller
+{
+    public function searchAction(Request $request)
+    {
+        $searcher = $this->get('shuba_blog.search');
+        $result = $searcher->search($request->get('search'));
+        $repository = $this->getDoctrine()->getRepository('ShubaBlogBundle:Message');
+        $query = $repository->createQueryBuilder('message')
+            ->where('message.id IN (:ids)')
+            ->setParameter('ids', $result)
+            ->getQuery();
+        $messages = $query->getResult();
+
+        if (!$messages){
+            throw $this->createNotFoundException('Opss, dont search');
+        }
+
+        return $this->render('ShubaBlogBundle:Search:search.html.twig', array('messages' => $messages));
+    }
+
+}
